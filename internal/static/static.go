ce6a9dd3f6c9f5d4e1dd355df870e8df92f3c8c2
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-12-30 23:09:46 +0800

    fix static site redirect all page to home bug

--- internal/static/static.go
+++ internal/static/static.go
@@ -6,7 +6,7 @@ func GenerateDockerfile(meta types.PlanMeta) (string, error) {
 	dockerfile := `FROM nginx:alpine
 WORKDIR /static
 COPY . . 
-RUN echo "server { listen 8080; root /static; location / {try_files \$uri /index.html; }}"> /etc/nginx/conf.d/default.conf
+RUN echo "server { listen 8080; root /static; }"> /etc/nginx/conf.d/default.conf
 EXPOSE 8080`
 
 	return dockerfile, nil
