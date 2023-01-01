83000e9364cec0153e57ac79c73b55a05c29ddf5
Yuanlin Lin <im.yuanlinlin@gmail.com>
2023-01-01 18:55:05 +0800

    add Node.js Puppeteer support (all missing libs)

--- internal/nodejs/node.go
+++ internal/nodejs/node.go
@@ -20,7 +20,7 @@ RUN pnpm install
 	needPuppeteer := meta["needPuppeteer"] == "true"
 	if needPuppeteer {
 		installCmd += `
-RUN apt-get update && apt-get install -y libnss3 libgconf-2-4 libatk-bridge2.0-0 libcups2 
+RUN apt-get update && apt-get install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libgbm1 libasound2 libpangocairo-1.0-0 libxss1 libgtk-3-0 libxshmfence1 libglu1
 `
 	}
 
