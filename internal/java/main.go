952650ad811038a1cf74ffff3f895ac60f424b1e
Yuanlin Lin <im.yuanlinlin@gmail.com>
2023-01-05 11:17:30 +0800

    fix Java dockerfile for gradle

--- internal/java/main.go
+++ internal/java/main.go
@@ -4,29 +4,57 @@ import "github.com/zeabur/zbpack/pkg/types"
 
 func GenerateDockerfile(meta types.PlanMeta) (string, error) {
 	projectType := meta["type"]
+	framework := meta["framework"]
+
+	isMaven := projectType == string(types.JavaProjectTypeMaven)
+	isGradle := projectType == string(types.JavaProjectTypeGradle)
+	isSpringBoot := framework == string(types.JavaFrameworkSpringBoot)
+
+	// TODO: select the correct base image jdk version
+	baseImage := "openjdk:8-jdk-alpine"
+	if isMaven {
+		baseImage = "maven:latest"
+	}
+	if isGradle {
+		baseImage = "gradle:latest"
+	}
 
 	dockerfile := ""
 
 	switch projectType {
 	case string(types.JavaProjectTypeMaven):
-		dockerfile += `FROM maven:3.6.3-openjdk-15 as builder
+		dockerfile += `FROM ` + baseImage + ` 
 WORKDIR /src
 COPY . .
-RUN mvn install
+RUN mvn clean dependency:list install
 `
 	case string(types.JavaProjectTypeGradle):
-		dockerfile += `FROM gradle:6.8.3-jdk15 as builder
+		dockerfile += `FROM ` + baseImage + `
 WORKDIR /src
 COPY . .
 RUN gradle build
 `
 	}
 
-	dockerfile += `FROM openjdk:15 as runtime
-WORKDIR /app
-COPY --from=builder /src/target/*.jar .
-CMD java -jar *.jar
-`
+	startCmd := ""
+
+	if isMaven {
+		startCmd = "CMD java -jar target/*.jar"
+	}
+
+	if isGradle {
+		startCmd = "CMD java -jar build/libs/*.jar"
+	}
+
+	if isMaven && isSpringBoot {
+		startCmd = "CMD java -Dserver.port=$PORT -jar target/*.jar"
+	}
+
+	if isGradle && isSpringBoot {
+		startCmd = "CMD java -Dserver.port=$PORT -jar build/libs/*.jar"
+	}
+
+	dockerfile += startCmd
 
 	return dockerfile, nil
 }
