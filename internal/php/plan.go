7797069e525fab39805d9b39d723b1f3c61fa208
Yuanlin Lin <im.yuanlinlin@gmail.com>
2023-01-26 00:40:48 +0800

    remove unused code in Plan phase of PHP

--- internal/php/plan.go
+++ internal/php/plan.go
@@ -68,143 +68,12 @@ func GetPhpVersion(absPath string) string {
 	return "8.1"
 }
 
-func WriteConfigForPhpImage(absPath string) bool {
-	path := absPath + "docker/"
-	err := os.Mkdir(path, os.ModePerm)
-	if err != nil {
-		fmt.Println(err)
-	}
-	nginxFile, err := os.Create(path + "nginx.conf")
-	if err != nil {
-		fmt.Println(err)
-		return false
-	}
-	defer nginxFile.Close()
-	nginxFile.WriteString(`server {
-	listen 8080;
-	root /var/www/public;
-
-	add_header X-Frame-Options "SAMEORIGIN";
-	add_header X-Content-Type-Options "nosniff";
-
-	index index.php index.html;
-	charset utf-8;
-
-	location = /favicon.ico { access_log off; log_not_found off; }
-	location = /robots.txt  { access_log off; log_not_found off; }
-
-	error_page 404 /index.php;
-
-	location ~ \.php$ {
-		try_files $uri =404;
-		fastcgi_split_path_info ^(.+\.php)(/.+)$;
-		fastcgi_pass 127.0.0.1:9000;
-		fastcgi_index index.php;
-		include fastcgi_params;
-		fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-		fastcgi_param PATH_INFO $fastcgi_path_info;
-		fastcgi_buffering off;
-	}
-
-	location / {
-		try_files $uri $uri/ /index.php?$query_string;
-		gzip_static on;
-	}
-	
-	location ~ /\.(?!well-known).* {
-		deny all;
-	}
-	}`)
-	supervisorFile, err := os.Create(path + "supervisor.conf")
-	if err != nil {
-		fmt.Println(err)
-		return false
-	}
-	defer supervisorFile.Close()
-	supervisorFile.WriteString(
-		`[supervisord]
-nodaemon=true
-loglevel = info
-logfile=/var/log/supervisord.log
-pidfile=/var/run/supervisord.pid
-
-[group:laravel-worker]
-priority=999
-programs=nginx,php8-fpm,laravel-schedule,laravel-notification,laravel-queue
-
-[program:nginx]
-priority=10
-autostart=true
-autorestart=true
-stderr_logfile_maxbytes=0
-stdout_logfile_maxbytes=0
-stdout_events_enabled=true
-stderr_events_enabled=true
-command=/usr/sbin/nginx -g 'daemon off;'
-stderr_logfile=/var/log/nginx/error.log
-stdout_logfile=/var/log/nginx/access.log
-
-[program:php8-fpm]
-priority=5
-autostart=true
-autorestart=true
-stderr_logfile_maxbytes=0
-stdout_logfile_maxbytes=0
-command=/usr/local/sbin/php-fpm -R
-stderr_logfile=/var/log/nginx/php-error.log
-stdout_logfile=/var/log/nginx/php-access.log
-
-[program:laravel-schedule]
-numprocs=1
-autostart=true
-autorestart=true
-redirect_stderr=true
-process_name=%(program_name)s_%(process_num)02d
-command=php /var/www/artisan schedule:run
-stdout_logfile=/var/log/nginx/schedule.log
-
-[program:laravel-notification]
-numprocs=1
-autostart=true
-autorestart=true
-redirect_stderr=true
-process_name=%(program_name)s_%(process_num)02d
-command=php /var/www/artisan notification:worker
-stdout_logfile=/var/log/nginx/notification.log
-
-[program:laravel-queue]
-numprocs=5
-autostart=true
-autorestart=true
-redirect_stderr=true
-process_name=%(program_name)s_%(process_num)02d
-stdout_logfile=/var/log/nginx/worker.log
-command=php /var/www/artisan queue:work sqs --sleep=3 --tries=3`)
-
-	phpiniFile, err := os.Create(path + "php.ini")
-	if err != nil {
-		fmt.Println(err)
-		return false
-	}
-	defer phpiniFile.Close()
-	phpiniFile.WriteString(`
-log_errors=1
-display_errors=1
-post_max_size=40M
-upload_max_filesize=40M
-display_startup_errors=1
-error_log=/var/log/php/errors.log`)
-	return true
-}
-
 func DetermineProjectFramework(absPath string) PhpFramework {
 	composerJsonMarshal, err := os.ReadFile(path.Join(absPath, "composer.json"))
-
-	WriteConfigForPhpImage(absPath)
-
 	if err != nil {
 		return PhpFrameworkNone
 	}
+
 	composerJson := struct {
 		Name       string            `json:"name"`
 		Require    map[string]string `json:"require"`
@@ -213,6 +82,7 @@ func DetermineProjectFramework(absPath string) PhpFramework {
 	if err := json.Unmarshal(composerJsonMarshal, &composerJson); err != nil {
 		return PhpFrameworkNone
 	}
+
 	if _, isLaravel := composerJson.Require["laravel/framework"]; isLaravel {
 		return PhpFrameworkLaravel
 	}
