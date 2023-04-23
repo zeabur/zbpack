58f1f22b1ad4541a940e1d7ddf3b3840b5ca6561
Yuanlin Lin <im.yuanlinlin@gmail.com>
2023-04-23 21:00:14 +0800

    remove cacheFrom

--- pkg/zeaburpack/image.go
+++ pkg/zeaburpack/image.go
@@ -70,11 +70,6 @@ func buildImage(opt *BuildImageOptions) error {
 		dockerCmd = append(dockerCmd, "--progress", "tty")
 	}
 
-	if opt.CacheFrom != nil && len(*opt.CacheFrom) > 0 {
-		dockerCmd = append(dockerCmd, "--cache-from", *opt.CacheFrom)
-		dockerCmd = append(dockerCmd, "--build-arg", "BUILDKIT_INLINE_CACHE=1")
-	}
-
 	dockerCmd = append(dockerCmd, opt.AbsPath)
 
 	cmd := exec.Command("docker", dockerCmd...)
