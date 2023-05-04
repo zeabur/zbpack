63dccd097b6c19b9f6582d64ad09f55d3e73fa81
Yuanlin Lin <im.yuanlinlin@gmail.com>
2023-05-04 14:25:31 +0800

    fix static provider not output `.zeabur/output/static`

--- internal/static/static.go
+++ internal/static/static.go
@@ -3,10 +3,10 @@ package static
 import "github.com/zeabur/zbpack/pkg/types"
 
 func GenerateDockerfile(meta types.PlanMeta) (string, error) {
-	dockerfile := `FROM docker.io/library/nginx:alpine
-WORKDIR /static
+	dockerfile := `FROM docker.io/library/nginx:alpine as runtime
+WORKDIR /usr/share/nginx/html/static
 COPY . . 
-RUN echo "server { listen 8080; root /static; }"> /etc/nginx/conf.d/default.conf
+RUN echo "server { listen 8080; root /usr/share/nginx/html/static; }"> /etc/nginx/conf.d/default.conf
 EXPOSE 8080`
 
 	return dockerfile, nil
