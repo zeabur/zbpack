a38e3684b523abb479dfe0443d7ffc01c55dfc90
Yi-Jyun Pan <yjpan@zeabur.com>
2023-06-24 21:49:43 +0800

    refactor(planner/php): Refactor and add tests

---
+++ internal/php/composer.go
@@ -0,0 +1,43 @@
+package php
+
+import (
+	"encoding/json"
+
+	"github.com/spf13/afero"
+)
+
+type composerJSONSchema struct {
+	Name       string             `json:"name"`
+	Require    *map[string]string `json:"require"`
+	RequireDev *map[string]string `json:"require-dev"`
+}
+
+func (c *composerJSONSchema) GetRequire(dep string) (string, bool) {
+	if c.Require == nil {
+		return "", false
+	}
+	v, ok := (*c.Require)[dep]
+	return v, ok
+}
+
+func (c *composerJSONSchema) GetRequireDev(dep string) (string, bool) {
+	if c.RequireDev == nil {
+		return "", false
+	}
+	v, ok := (*c.RequireDev)[dep]
+	return v, ok
+}
+
+func parseComposerJSON(source afero.Fs) (composerJSONSchema, error) {
+	composerJSONMarshal, err := afero.ReadFile(source, "composer.json")
+	if err != nil {
+		return composerJSONSchema{}, err
+	}
+	var composerJSON composerJSONSchema
+
+	if err := json.Unmarshal(composerJSONMarshal, &composerJSON); err != nil {
+		return composerJSONSchema{}, err
+	}
+
+	return composerJSON, nil
+}
