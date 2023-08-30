b63f2e2978b4825eca92c774007ed9f715d05b5e
Yuanlin Lin <im.yuanlinlin@gmail.com>
2023-08-30 17:22:48 +0800

    fix(planner/ruby): Fix ruby version trailing quote

--- internal/ruby/plan.go
+++ internal/ruby/plan.go
@@ -3,20 +3,36 @@ package ruby
 import (
 	"github.com/spf13/afero"
 	"github.com/zeabur/zbpack/pkg/types"
+	"regexp"
+	"strings"
 )
 
 // DetermineRubyVersion determines the version of Ruby used in the project.
 func DetermineRubyVersion(source afero.Fs) string {
-	RubyVersion := GetGemfileValue(source, `ruby "`)
+	reg := regexp.MustCompile(`ruby "(\d+\.\d+\.\d+)"`)
+	sourceFile, err := afero.ReadFile(source, "Gemfile")
+	if err != nil {
+		return ""
+	}
 
-	return RubyVersion
+	matches := reg.FindStringSubmatch(string(sourceFile))
+	if len(matches) < 2 {
+		return ""
+	}
+
+	return matches[1]
 }
 
 // DetermineRubyFramework determines the framework of the Ruby project.
 func DetermineRubyFramework(source afero.Fs) types.RubyFramework {
-	railsVersion := GetGemfileValue(source, `rails`)
-	if railsVersion != "" {
+	f, err := afero.ReadFile(source, "Gemfile")
+	if err != nil {
+		return types.RubyFrameworkNone
+	}
+
+	if strings.Contains(string(f), "rails") {
 		return types.RubyFrameworkRails
 	}
+
 	return types.RubyFrameworkNone
 }
