0b51678fc5e3cf98ec109750f11f805706c8a916
Yi-Jyun Pan <yjpan@zeabur.com>
2023-10-03 18:13:54 +0800

    test(planner/config): GetProjectConfigValue

--- pkg/plan/config_test.go
+++ pkg/plan/config_test.go
@@ -3,6 +3,7 @@ package plan_test
 import (
 	"testing"
 
+	"github.com/moznion/go-optional"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 	"github.com/zeabur/zbpack/pkg/plan"
@@ -43,4 +44,77 @@ func TestProjectConfiguration_ZbpackTomlNotExisted(t *testing.T) {
 	assert.False(t, config.IsSet("laravel"))
 	assert.False(t, config.IsSet("laravel.test"))
 	assert.False(t, config.IsSet("laravel.owo"))
+}
+
+func TestGetProjectConfigValue_Global(t *testing.T) {
+	t.Parallel()
+
+	fs := afero.NewMemMapFs()
+	_ = afero.WriteFile(fs, "zbpack.toml", []byte(`[project]
+build_command = "build"`), 0644)
+
+	config := plan.NewProjectConfigurationFromFs(fs)
+	assert.Equal(t, optional.Some("build"), plan.GetProjectConfigValue(config, "", "build_command"))
+}
+
+func TestGetProjectConfigValue_Submodule(t *testing.T) {
+	t.Parallel()
+
+	fs := afero.NewMemMapFs()
+	_ = afero.WriteFile(fs, "zbpack.toml", []byte(`[project.sm]
+build_command = "build.sm"`), 0644)
+
+	config := plan.NewProjectConfigurationFromFs(fs)
+	assert.Equal(t, optional.Some("build.sm"), plan.GetProjectConfigValue(config, "sm", "build_command"))
+}
+
+func TestGetProjectConfigValue_SubmoduleOverride(t *testing.T) {
+	t.Parallel()
+
+	fs := afero.NewMemMapFs()
+	_ = afero.WriteFile(fs, "zbpack.toml", []byte(`[project]
+build_command = "build"
+[project.sm]
+build_command = "build.sm"`), 0644)
+
+	config := plan.NewProjectConfigurationFromFs(fs)
+	assert.Equal(t, optional.Some("build.sm"), plan.GetProjectConfigValue(config, "sm", "build_command"))
+}
+
+func TestGetProjectConfigValue_SubmoduleFallback(t *testing.T) {
+	t.Parallel()
+
+	t.Run("with-submodule-group", func(t *testing.T) {
+		t.Parallel()
+
+		fs := afero.NewMemMapFs()
+		_ = afero.WriteFile(fs, "zbpack.toml", []byte(`[project]
+build_command = "build"
+[project.sm]`), 0644)
+
+		config := plan.NewProjectConfigurationFromFs(fs)
+		assert.Equal(t, optional.Some("build"), plan.GetProjectConfigValue(config, "sm", "build_command"))
+	})
+
+	t.Run("without-submodule-group", func(t *testing.T) {
+		t.Parallel()
+
+		fs := afero.NewMemMapFs()
+		_ = afero.WriteFile(fs, "zbpack.toml", []byte(`[project]
+build_command = "build"`), 0644)
+
+		config := plan.NewProjectConfigurationFromFs(fs)
+		assert.Equal(t, optional.Some("build"), plan.GetProjectConfigValue(config, "sm", "build_command"))
+	})
+}
+
+func TestGetProjectConfigValue_None(t *testing.T) {
+	t.Parallel()
+
+	fs := afero.NewMemMapFs()
+	_ = afero.WriteFile(fs, "zbpack.toml", []byte(`[project]
+`), 0644)
+
+	config := plan.NewProjectConfigurationFromFs(fs)
+	assert.Equal(t, optional.None[string](), plan.GetProjectConfigValue(config, "", "build_command"))
 }
