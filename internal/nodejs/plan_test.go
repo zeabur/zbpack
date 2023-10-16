a71a86f7e21360bdfb5a3a9262e80c85de510c07
Yi-Jyun Pan <yjpan@zeabur.com>
2023-10-16 15:17:48 +0800

    test(planner/nodejs): Update API & Add more tests

--- internal/nodejs/plan_test.go
+++ internal/nodejs/plan_test.go
@@ -3,64 +3,65 @@ package nodejs
 import (
 	"testing"
 
-	"github.com/Masterminds/semver/v3"
 	"github.com/stretchr/testify/assert"
 )
 
-func getMockVersionsList() []*semver.Version {
-	rawVer := []string{
-		"v20",
-		"v19",
-		"v18",
-		"v17",
-		"v16",
-		"v15",
-		"v14",
-		"v13",
-		"v12",
-		"v10",
-	}
-
-	versions := make([]*semver.Version, len(rawVer))
-
-	for i, v := range rawVer {
-		ver, err := semver.NewVersion(v)
-		if err != nil {
-			panic(err)
-		}
-
-		versions[i] = ver
-	}
-
-	return versions
-}
-
 func TestGetNodeVersion_Empty(t *testing.T) {
-	v := getNodeVersion("", getMockVersionsList())
+	v := getNodeVersion("")
 	assert.Equal(t, defaultNodeVersion, v)
 }
 
 func TestGetNodeVersion_Fixed(t *testing.T) {
-	v := getNodeVersion("10", getMockVersionsList())
+	v := getNodeVersion("10")
 	assert.Equal(t, "10", v)
 }
 
 func TestGetNodeVersion_Or(t *testing.T) {
-	v := getNodeVersion("^10 || ^12 || ^14", getMockVersionsList())
+	v := getNodeVersion("^10 || ^12 || ^14")
 	assert.Equal(t, "14", v)
 }
 
 func TestGetNodeVersion_GreaterThanWithLessThan(t *testing.T) {
-	v := getNodeVersion(">=16 <=20", getMockVersionsList())
+	v := getNodeVersion(">=16 <=20")
 	assert.Equal(t, "20", v)
 }
 
 func TestGetNodeVersion_GreaterThan(t *testing.T) {
-	v := getNodeVersion(">=4", getMockVersionsList())
+	v := getNodeVersion(">=4")
 	assert.Equal(t, "20", v)
 }
 
 func TestGetNodeVersion_LessThan(t *testing.T) {
-	v := getNodeVersion("<18", getMockVersionsList())
+	v := getNodeVersion("<18")
 	assert.Equal(t, "17", v)
+}
+
+func TestGetNodeVersion_Exact(t *testing.T) {
+	v := getNodeVersion("16.0.0")
+	assert.Equal(t, "16.0.0", v)
+}
+
+func TestGetNodeVersion_Exact_WithEqualOp(t *testing.T) {
+	v := getNodeVersion("=16.0.0")
+	assert.Equal(t, "16.0.0", v)
+}
+
+func TestGetNodeVersion_CaretMinor(t *testing.T) {
+	v := getNodeVersion("^16.1.0")
+	assert.Equal(t, "16", v)
+}
+
+func TestGetNodeVersion_TildeMinor(t *testing.T) {
+	v := getNodeVersion("~16.0.1")
+	assert.Equal(t, "16.0", v)
+}
+
+func TestGetNodeVersion_ExactWithWildcard(t *testing.T) {
+	v := getNodeVersion("16.0.*")
+	assert.Equal(t, "16.0", v)
+}
+
+func TestGetNodeVersion_TildeWithWildcard(t *testing.T) {
+	v := getNodeVersion("~16.*")
+	assert.Equal(t, "16", v)
 }
