2af7dd7d3602197f2e03360c7e58a63a5ad295b5
hackerchai <i@hackerchai.com>
2023-10-31 14:11:13 +0800

    Revert "fix(zbpack): Fix dockerignore logic not working"
    This reverts commit 9dd90e3a01578be05e7084b177849660c166219b.

--- pkg/zeaburpack/image.go
+++ pkg/zeaburpack/image.go
@@ -3,7 +3,6 @@ package zeaburpack
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"math/rand"
 	"os"
 	"os/exec"
@@ -92,7 +91,6 @@ func buildImage(opt *buildImageOptions) error {
 	newDockerfile := strings.Join(lines, "\n")
 
 	tempDir := os.TempDir()
-	srcDIr := opt.AbsPath
 	buildID := strconv.Itoa(rand.Int())
 
 	err := os.MkdirAll(path.Join(tempDir, buildID), 0o755)
@@ -107,46 +105,11 @@ func buildImage(opt *buildImageOptions) error {
 	}
 
 	dockerIgnore := []string{".next", "node_modules", ".zeabur"}
-	dockerIgnorePath := path.Join(srcDIr, ".dockerignore")
-	isDockerIgnoreExists := false
-	// if .dockerignore exists, we need to append the content to the end of the file
-	if fileExists(dockerIgnorePath) {
-		isDockerIgnoreExists = true
-		err := backupFile(dockerIgnorePath, dockerIgnorePath+".bak")
-		if err != nil {
-			return fmt.Errorf("copy .dockerignore: %w", err)
-		}
-		dockerIgnoreFile, err := os.ReadFile(path.Join(srcDIr, ".dockerignore"))
-		if err != nil {
-			return fmt.Errorf("read .dockerignore: %w", err)
-		}
-		dockerIgnore = append(dockerIgnore, strings.Split(string(dockerIgnoreFile), "\n")...)
-		err = appendToFile(dockerIgnorePath, strings.Join(dockerIgnore, "\n"))
-		if err != nil {
-			return fmt.Errorf("append .dockerignore: %w", err)
-		}
-	}
-	// if .dockerignore does not exist, we need to create it
+	dockerIgnorePath := path.Join(tempDir, buildID, ".dockerignore")
 	err = os.WriteFile(dockerIgnorePath, []byte(strings.Join(dockerIgnore, "\n")), 0o644)
 	if err != nil {
 		return fmt.Errorf("write .dockerignore: %w", err)
 	}
-
-	defer func() {
-		if !isDockerIgnoreExists {
-			// remove the .dockerignore file we created
-			err := os.Remove(dockerIgnorePath)
-			if err != nil {
-				println("failed to remove .dockerignore: " + err.Error())
-			}
-		} else {
-			// restore the .dockerignore file
-			err = os.Rename(dockerIgnorePath+".bak", dockerIgnorePath)
-			if err != nil {
-				println("failed to restore .dockerignore: " + err.Error())
-			}
-		}
-	}()
 
 	dockerCmd := []string{
 		"buildx",
@@ -214,45 +177,4 @@ func buildImage(opt *buildImageOptions) error {
 	}()
 
 	return cmd.Wait()
-}
-
-func fileExists(filename string) bool {
-	_, err := os.Stat(filename)
-	return !os.IsNotExist(err)
-}
-
-func appendToFile(filename, content string) error {
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = file.WriteString(content)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func backupFile(sourceFile, destinationFile string) error {
-	source, err := os.Open(sourceFile)
-	if err != nil {
-		return err
-	}
-	defer source.Close()
-
-	destination, err := os.Create(destinationFile)
-	if err != nil {
-		return err
-	}
-	defer destination.Close()
-
-	_, err = io.Copy(destination, source)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
