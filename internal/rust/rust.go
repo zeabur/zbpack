b6f489c1be0de4ae93d41400323aa93035ac2961
hackerchai <i@hackerchai.com>
2024-02-02 16:27:15 +0800

    feat(plan/rust): Implement rust serverless
    Signed-off-by: hackerchai <i@hackerchai.com>

--- internal/rust/rust.go
+++ internal/rust/rust.go
@@ -3,14 +3,9 @@ package rust
 
 import (
 	"bytes"
-	"log"
-	"os"
-	"strings"
 	"text/template"
 
 	_ "embed"
-
-	"github.com/spf13/afero"
 
 	"github.com/zeabur/zbpack/pkg/packer"
 	"github.com/zeabur/zbpack/pkg/types"
@@ -18,46 +13,6 @@ import (
 
 //go:embed template.Dockerfile
 var templateDockerfile string
-
-// GetMetaOptions is the options for GetMeta.
-type GetMetaOptions struct {
-	Src afero.Fs
-
-	// In Rust, the submodule name is the binary name.
-	SubmoduleName string
-}
-
-func needOpenssl(source afero.Fs) bool {
-	for _, file := range []string{"Cargo.toml", "Cargo.lock"} {
-		file, err := afero.ReadFile(source, file)
-		if err != nil {
-			if !os.IsNotExist(err) {
-				log.Println(err)
-			}
-			continue
-		}
-
-		if strings.Contains(string(file), "openssl") {
-			return true
-		}
-	}
-	return false
-}
-
-// GetMeta gets the metadata of the Rust project.
-func GetMeta(options GetMetaOptions) types.PlanMeta {
-	var opensslFlag string
-	if needOpenssl(options.Src) {
-		opensslFlag = "yes"
-	} else {
-		opensslFlag = "no"
-	}
-
-	return types.PlanMeta{
-		"BinName":     options.SubmoduleName,
-		"NeedOpenssl": opensslFlag,
-	}
-}
 
 // GenerateDockerfile generates the Dockerfile for the Rust project.
 func GenerateDockerfile(meta types.PlanMeta) (string, error) {
