e11b35ecccfb72320fa23ac04a601f67f15358e0
Yi-Jyun Pan <yjpan@zeabur.com>
2024-02-06 12:20:41 +0800

    fix(planner/php): Install install-php-extensions only if we need

--- internal/php/php.go
+++ internal/php/php.go
@@ -30,9 +30,10 @@ func GenerateDockerfile(meta types.PlanMeta) (string, error) {
 RUN apt-get update && apt-get install -y %s && rm -rf /var/lib/apt/lists/*
 `, meta["deps"])
 	if projectProperty&types.PHPPropertyComposer != 0 {
-		installCMD += `
-RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-ADD https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions /usr/local/bin/
+		installCMD += "RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer\n"
+	}
+	if meta["exts"] != "" {
+		installCMD += `ADD https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions /usr/local/bin/
 RUN chmod +x /usr/local/bin/install-php-extensions && sync
 `
 	}
