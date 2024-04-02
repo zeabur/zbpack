e3c0ce44cd7bff256dc60c3e99bc3d4cb9a152e3
Yuanlin Lin <lin@yual.in>
2024-04-02 11:27:46 +0800

    feat: add gleam support (#242)
    * feat: add gleam support
    
    * fix: typo
    
    * fix: typo
    
    * fix: typo
    
    * fix: ci

---
+++ internal/gleam/identify.go
@@ -0,0 +1,31 @@
+package gleam
+
+import (
+	"github.com/spf13/afero"
+
+	"github.com/zeabur/zbpack/internal/utils"
+	"github.com/zeabur/zbpack/pkg/plan"
+	"github.com/zeabur/zbpack/pkg/types"
+)
+
+type identify struct{}
+
+// NewIdentifier returns a new Gleam identifier.
+func NewIdentifier() plan.Identifier {
+	return &identify{}
+}
+
+func (i *identify) PlanType() types.PlanType {
+	return types.PlanTypeGleam
+}
+
+// Match returns true if gleam.toml is found in the source
+func (i *identify) Match(fs afero.Fs) bool {
+	return utils.HasFile(fs, "gleam.toml")
+}
+
+func (i *identify) PlanMeta(_ plan.NewPlannerOptions) types.PlanMeta {
+	return types.PlanMeta{}
+}
+
+var _ plan.Identifier = (*identify)(nil)
