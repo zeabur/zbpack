1359345a3d214ddfdd2c5ee35539ff52c7ef05e7
Yuanlin Lin <im.yuanlinlin@gmail.com>
2024-04-17 17:38:12 +0800

    fix: add elixir to gleam builder

--- internal/gleam/gleam.go
+++ internal/gleam/gleam.go
@@ -9,6 +9,9 @@ import (
 // GenerateDockerfile generates the Dockerfile for Gleam projects.
 func GenerateDockerfile(_ types.PlanMeta) (string, error) {
 	return `FROM ghcr.io/gleam-lang/gleam:v1.0.0-erlang-alpine
+RUN apk add --no-cache elixir
+RUN mix local.hex --force
+RUN mix local.rebar --force
 COPY . /build/
 RUN cd /build \
   && gleam export erlang-shipment \
