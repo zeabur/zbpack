3d2d1aecaf11b3620129dea655acf72915ade9ce
pan93412 <pan93412@gmail.com>
2024-06-09 12:31:22 +0000

    fix(planner/php): Correct /var/www permission

--- internal/php/php.go
+++ internal/php/php.go
@@ -40,13 +40,15 @@ RUN install-php-extensions ` + meta["exts"] + "\n"
 
 	// copy source code to /var/www/public, which is Nginx root directory
 	copyCommand := `
-COPY --chown=www-data:www-data . /var/www/public
+RUN chown -R www-data:www-data /var/www
+COPY --chown=www-data:www-data --chmod=755 . /var/www/public
 WORKDIR /var/www/public
 `
 
 	if meta["framework"] != "none" {
 		copyCommand = `
-COPY --chown=www-data:www-data . /var/www
+RUN chown -R www-data:www-data /var/www
+COPY --chown=www-data:www-data --chmod=755 . /var/www
 WORKDIR /var/www
 `
 	}
