98fe0f0a342f94c5ae448df117b21d6a738fcaa8
Yi-Jyun Pan <yjpan@zeabur.com>
2024-06-27 13:03:10 +0800

    feat: Add ExplainableIdentifier and FieldInfo

---
+++ pkg/types/field_info.go
@@ -0,0 +1,75 @@
+package types
+
+import "fmt"
+
+// FieldInfo is the information of a field in a struct.
+type FieldInfo struct {
+	// Key is the key of the field in the struct.
+	Key string
+	// Name is the name of the field in the struct.
+	Name string
+	// Description is the description of the field in the struct.
+	Description string
+	// Icon is the icon of the field in the struct.
+	//
+	// It should be a URL of the icon.
+	Icon string
+}
+
+func NewPlanTypeFieldInfo(planType PlanType) FieldInfo {
+	return FieldInfo{
+		Key:         "_provider",
+		Name:        "Provider",
+		Description: "The programing language or runtime detected in the source code",
+		Icon:        fmt.Sprintf("https://raw.githubusercontent.com/zeabur/service-icons/main/git/%s/default.svg", planType),
+	}
+}
+
+func NewFrameworkFieldInfo(key string, planType PlanType, framework string) FieldInfo {
+	return FieldInfo{
+		Key:         key,
+		Name:        "Framework",
+		Description: "The framework detected in the source code",
+		Icon:        fmt.Sprintf("https://raw.githubusercontent.com/zeabur/service-icons/main/git/%s/%s.svg", planType, framework),
+	}
+}
+
+func NewOutputDirFieldInfo(key string) FieldInfo {
+	return FieldInfo{
+		Key:         key,
+		Name:        "Output Directory",
+		Description: "The directory where Zeabur will use as the output static files.",
+	}
+}
+
+func NewInstallCmdFieldInfo(key string) FieldInfo {
+	return FieldInfo{
+		Key:         key,
+		Name:        "Install Command",
+		Description: "The command to install the dependencies",
+	}
+}
+
+func NewBuildCmdFieldInfo(key string) FieldInfo {
+	return FieldInfo{
+		Key:         key,
+		Name:        "Build Command",
+		Description: "The command to build the source code",
+	}
+}
+
+func NewStartCmdFieldInfo(key string) FieldInfo {
+	return FieldInfo{
+		Key:         key,
+		Name:        "Start Command",
+		Description: "The command to start the service",
+	}
+}
+
+func NewServerlessFieldInfo(key string) FieldInfo {
+	return FieldInfo{
+		Key:         key,
+		Name:        "Serverless",
+		Description: "Whether the project is serverless",
+	}
+}
