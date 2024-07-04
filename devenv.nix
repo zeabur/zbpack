2d6a81234152ac8a31ba00c1201e4257428de088
Yi-Jyun Pan <yjpan@zeabur.com>
2024-07-04 18:22:11 +0800

    refactor: Migrate to Nix flake and devenv

---
+++ devenv.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.buildkit pkgs.gomod2nix pkgs.golangci-lint ];
+
+  # https://devenv.sh/scripts/
+
+  enterShell = ''
+    go mod download
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    go test ./...
+  '';
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  languages.go.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
