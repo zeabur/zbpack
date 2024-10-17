73bf0ec8410662afe4c3e0d4c4ebd0d448c28372
Yi-Jyun Pan <yjpan@zeabur.com>
2024-10-18 01:38:34 +0800

    refactor(dockerfiles): Rename to "composable"

--- internal/dockerfiles/dockerfiles.go
+++ internal/dockerfiles/dockerfiles.go
@@ -7,10 +7,10 @@ import (
 	"path"
 )
 
-//go:embed selectable
-var selectable embed.FS
+//go:embed composable
+var composable embed.FS
 
 // GetDockerfileContent returns the content of the Dockerfile with the given name.
 func GetDockerfileContent(name string) ([]byte, error) {
-	return selectable.ReadFile(path.Join("selectable", name+".Dockerfile"))
+	return composable.ReadFile(path.Join("composable", name+".Dockerfile"))
 }
