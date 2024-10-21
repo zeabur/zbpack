55cf896f17a7259295369d3bca7069026089675c
Yi-Jyun Pan <yjpan@zeabur.com>
2024-10-21 22:00:17 +0800

    refactor(tests): Use GitHub repository & Record plan meta

--- tests/node-solid-static/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import solid from "solid-start/vite";
-import staticAdapter from "solid-start-static";
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  plugins: [solid({
-	adapter: staticAdapter(),
-  })],
-});
