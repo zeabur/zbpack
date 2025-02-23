94d8090eb1472059409dace4b4ca923f9cd8c97c
Yi-Jyun Pan <yjpan@zeabur.com>
2025-02-23 23:22:18 +0800

    refactor(planner/dart): Remove serverless support

--- internal/dart/dart_test.go
+++ internal/dart/dart_test.go
@@ -10,29 +10,11 @@ import (
 func TestGenerateDockerfileStatic(t *testing.T) {
 	t.Parallel()
 
-	t.Run("flutter, serverless", func(t *testing.T) {
-		t.Parallel()
-
-		dockerfile, err := dart.GenerateDockerfile(map[string]string{
-			"framework":  "flutter",
-			"serverless": "true",
-		})
-
-		assert.NoError(t, err)
-		assert.Contains(t, dockerfile, "FROM scratch")
-		assert.NotContains(t, dockerfile, "FROM zeabur/caddy-static")
+	dockerfile, err := dart.GenerateDockerfile(map[string]string{
+		"framework": "flutter",
 	})
 
-	t.Run("flutter, non-serverless", func(t *testing.T) {
-		t.Parallel()
-
-		dockerfile, err := dart.GenerateDockerfile(map[string]string{
-			"framework":  "flutter",
-			"serverless": "false",
-		})
-
-		assert.NoError(t, err)
-		assert.Contains(t, dockerfile, "FROM scratch")
-		assert.Contains(t, dockerfile, "FROM zeabur/caddy-static")
-	})
+	assert.NoError(t, err)
+	assert.Contains(t, dockerfile, "FROM scratch")
+	assert.Contains(t, dockerfile, "FROM zeabur/caddy-static")
 }
