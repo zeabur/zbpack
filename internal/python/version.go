60677b80a2ff2d0698726be83e07d4ec2e42e089
Pan <pan93412@gmail.com>
2025-05-16 19:03:54 +0800

    chore(planner/python): upgrade to Python 3.13 by default

--- internal/python/version.go
+++ internal/python/version.go
@@ -4,7 +4,7 @@ import (
 	"github.com/zeabur/zbpack/internal/utils"
 )
 
-const defaultPython3Version = "3.10"
+const defaultPython3Version = "3.13"
 
 func getPython3Version(versionRange string) string {
 	return utils.ConstraintToVersion(versionRange, defaultPython3Version)
